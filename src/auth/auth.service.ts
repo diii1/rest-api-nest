700faeb8dd3134d70613c0faed8e45cb83aa4185
diii1 <bayu.setiaji709@gmail.com>
2022-07-04 15:26:12 +0700

    deps: add argon2 yarn dependencies

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
+  signup(dto: AuthDto) {
     return { msg: 'I have signed up' };
   }
 
