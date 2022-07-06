6fdb130bb646488556b4820e1342f9ae8a0f3f97
diii1 <bayu.setiaji709@gmail.com>
2022-07-06 09:17:47 +0700

    chore: config prisma with nestjs config

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'mysql://root:admin@localhost/nest',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
