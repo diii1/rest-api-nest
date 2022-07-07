3001546e52a61881fdb1cbdc8a5d0b23c31d7332
magang <bayu.setiaji709@gmail.com>
2022-07-07 09:13:59 +0700

    feat: get user with custom decorator and guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
+import { Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { Users } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
   @Get('me')
-  getMe(@Req() req: Request) {
-    return req.user;
+  getMe(@GetUser() user: Users) {
+    return user;
   }
+
+  // @Patch()
+  // editUser() {
+  //   return '';
+  // }
 }
